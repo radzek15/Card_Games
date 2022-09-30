eb792e8ef06410d13f6935f24e2aa8811809a184
Radzek15 <radzek15@gmail.com>
2022-09-30 23:29:22 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from random import shuffle
+values = {'2':2,'3':3,'4':4,'5':5,'6':6,'7':7,'8':8,'9':9,'10':10,'J':11,'Q':12,'K':13,'A':14}
+suits = ['Clubs','Hearts','Spades','Diamonds']
+ranks = [key for key in values.keys()]
+class Card:
+
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+class Deck:
+
+    def __init__(self):
+        self.deck = [Card(r,s) for r in ranks for s in suits]
